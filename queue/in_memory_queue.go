3d08575cfcdf027367c566f97ac547b75da4bb49
E. GOUINGUENET <elouangouinguenet@gmail.com>
2025-01-27 23:36:16 -0800

    batman start toooooo be in shape

--- queue/in_memory_queue.go
+++ queue/in_memory_queue.go
@@ -1,15 +1,26 @@
 package queue
 
+import "fmt"
+
 type (
 	SimpleMessage string
 	InMemoryQueue struct {
-		store []Message
+		store     []Message
+		queueType QueueType
 	}
+	QueueType int
 )
 
-func NewInMemoryQueue() *InMemoryQueue {
+const (
+	FIFO QueueType = iota
+	LIFO
+	Priority
+)
+
+func NewInMemoryQueue(queueType QueueType) *InMemoryQueue {
 	return &InMemoryQueue{
-		store: make([]Message, 0),
+		store:     make([]Message, 0),
+		queueType: queueType,
 	}
 }
 
@@ -18,4 +29,39 @@ func (q *InMemoryQueue) Size() int { return len(q.store) }
 func (q *InMemoryQueue) Enqueue(m Message) error {
 	q.store = append(q.store, m)
 	return nil
+}
+
+func (q *InMemoryQueue) Dequeue() (*Message, error) {
+	if q.Size() == 0 {
+		return nil, &EmptyQueueError{}
+	}
+
+	switch q.queueType {
+	case FIFO:
+		pop := q.store[0]
+		q.store = q.store[1:]
+		return &pop, nil
+	default:
+		return nil, &NotImplementedQueueType{queueType: q.queueType}
+	}
+}
+
+// Implementation of the error interface for the various custom error types.
+// EmptyQueueError is returned to signal that the queue is empty and that the
+// operation cannot be performed.
+type EmptyQueueError struct{}
+
+func (e *EmptyQueueError) Error() string {
+	return "Nothing to dequeue. Queue is empty."
+}
+
+// is returned to signal that the queue got QueueType that is either
+// unsupported, deprecated or simply not implemented. In theory this error
+// should not be returned often.
+type NotImplementedQueueType struct {
+	queueType QueueType
+}
+
+func (e *NotImplementedQueueType) Error() string {
+	return fmt.Sprintf("QueueType %2d is not implemented, supported or is deprecated.", e.queueType)
 }
--- queue/in_memory_queue_test.go
+++ queue/in_memory_queue_test.go
@@ -8,16 +8,28 @@ import (
 )
 
 func Test_NewInMemoryQueue(t *testing.T) {
-	require.NotNil(t, queue.NewInMemoryQueue(), "unexpected: NewInMemoryQueue() returned nil.")
+	require.NotNil(t, queue.NewInMemoryQueue(queue.FIFO), "unexpected: NewInMemoryQueue() returned nil.")
 }
 
 func Test_InMemoryQueue(t *testing.T) {
-	q := queue.NewInMemoryQueue()
+	q := queue.NewInMemoryQueue(queue.FIFO)
+	m0 := queue.SimpleMessage("Anakin was supposed to bring balance to force, not to join them.")
 	require.Equal(t, 0, q.Size(), "InMemoryQueue should have a size of 0 upon instanciation.")
-	q.Enqueue("Anakin was supposed to bring balance to force, not to join them.")
+	q.Enqueue(m0)
 	require.Equal(t, 1, q.Size(), "InMemoryQueue should have a size of 1 after enqueueing one message.")
 	q.Enqueue("Do or do not, there is no try.")
 	q.Enqueue("Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.")
 	q.Enqueue("When I left you, I was but the learner. Now I am the master.")
 	require.Equal(t, 4, q.Size(), "InMemoryQueue should have a size of 4 after 4 enqueues.")
+
+	actual, err := q.Dequeue()
+	require.Equal(t, m0, *actual, "In FIFO, the first queued message is expected to the first dequeued.")
+	require.Nil(t, err, "Dequeue a FIFO queue of size 4 should not return an error.")
+}
+
+func Test_InMemoryQueueEmptyQueueError(t *testing.T) {
+	q := queue.NewInMemoryQueue(queue.FIFO)
+	m, err := q.Dequeue()
+	require.Nil(t, m, "Dequeueing an empty queue should return a nil pointer for the message")
+	require.ErrorIs(t, err, &queue.EmptyQueueError{}, "")
 }
