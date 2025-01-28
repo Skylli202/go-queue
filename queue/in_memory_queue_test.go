dbc23b86f1ffa4890b5c9219f4d786efb77895ad
E. GOUINGUENET <elouangouinguenet@gmail.com>
2025-01-27 22:25:58 -0800

    batman > all.

--- queue/in_memory_queue_test.go
+++ queue/in_memory_queue_test.go
@@ -16,4 +16,8 @@ func Test_InMemoryQueue(t *testing.T) {
 	require.Equal(t, 0, q.Size(), "InMemoryQueue should have a size of 0 upon instanciation.")
 	q.Enqueue("Anakin was supposed to bring balance to force, not to join them.")
 	require.Equal(t, 1, q.Size(), "InMemoryQueue should have a size of 1 after enqueueing one message.")
+	q.Enqueue("Do or do not, there is no try.")
+	q.Enqueue("Fear is the path to the dark side. Fear leads to anger. Anger leads to hate. Hate leads to suffering.")
+	q.Enqueue("When I left you, I was but the learner. Now I am the master.")
+	require.Equal(t, 4, q.Size(), "InMemoryQueue should have a size of 4 after 4 enqueues.")
 }
